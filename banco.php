5b2d0414fbb7e19026ff3746f3765eba0726047e
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-10-31 21:11:13 -0300

    set e get endereco

--- banco.php
+++ banco.php
@@ -4,6 +4,7 @@ class Conta{
 
     private $numero;
     private $cliente;
+    private $endereco;
 
     public function setNumero($valor){
         $this->numero = $valor;
@@ -21,14 +22,23 @@ class Conta{
     public function getCliente(){
         return $this->cliente;
     }
+
+    public function setEndereco($rua){
+        $this->endereco = $rua;
+    }
+    public function getEndereco(){
+        return $this->endereco;
+    
+    }
 }
 
 $pessoa1 = new Conta;
 $pessoa1->setNumero("0000055-8");
 $pessoa1->setCliente('premium');
+$pessoa1->setEndereco('Almeria');
 
 echo "O numero da conta é ". $pessoa1->getNumero()."<br>";
 echo "Seu plano é ". $pessoa1->getCliente()."<br>";
-
+echo "Sua Rua é ". $pessoa1->getEndereco()."<br>";
 
 ?>
