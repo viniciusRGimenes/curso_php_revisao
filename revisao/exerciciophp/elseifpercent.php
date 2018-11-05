5f3b1ae5335d8d5e212c8bd4273a59dc35ec6789
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-05 19:48:12 -0200

    elseifporcent

---
+++ revisao/exerciciophp/elseifpercent.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "Jhon";
+$salario = 1;
+
+
+if($salario <= 1000){
+    echo "Você é muito pobre pra pagar imposto";
+}elseif($salario <= 1500){
+    echo "Você dara o dizimo para o governo";
+}elseif($salario <= 2000){
+    echo "Manda 15% para o goerno";
+}else{
+    echo "Manda 20% porque você tem muito";
+}
+
+?>
