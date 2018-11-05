b473b71708fdc2abf2b2dbc060e3d0d21cbe3876
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-05 21:19:40 -0200

    switch

---
+++ revisao/exerciciophp/switch.php
@@ -0,0 +1,26 @@
+<?php
+ 
+$opcao = 2;
+
+switch($opcao){
+    case(1):
+        echo "Burguer simples";
+        break;
+    case(2):
+        echo "Burguer com queijo";
+        break;
+    case(3):
+        echo "Burguer com queijo e fritas";
+        break;
+    case(4):
+        echo "Burguer simples com fritas";
+        break;
+    case(5):
+        echo "Burguer com queijo e Coca-Cola";
+        break;
+    case(6):
+        echo "Burguer  e Coca-Cola";
+        break;
+}
+
+?>
