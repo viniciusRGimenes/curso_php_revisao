8594f19d96ee8026bbad5a642c272af542cab9c1
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-05 19:18:25 -0200

    variaveis

---
+++ revisao/exerciciophp/variaveis.php
@@ -0,0 +1,22 @@
+<?php
+
+$nome =  "Vinicius";
+$sobrenome = "Gimenes";
+$endereço = "Rua Carilau Cerri";
+$idade = "24 anos";
+$telefone = "(11)XXXXX-XXXX";
+
+
+echo "meu nome é ";
+echo  $nome. " ";
+echo  $sobrenome . "<br>";
+echo " Morei na ";
+echo $endereço. "<br>";
+echo "Tenho ";
+echo $idade . "<br>";
+echo "Meu numero é ";
+echo $telefone;
+
+
+
+?>
