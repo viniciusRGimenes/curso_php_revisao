1250e2b8ffff29df82811014faf8b3aa80390305
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-07 18:07:36 -0200

    listar 10 funcionarios

---
+++ revisao/exerciciophp/listar10f.php
@@ -0,0 +1,17 @@
+<?php
+
+$funcionarios = array("Jhon", "Michael", "Charles", "Daniel", "Paulo", "Moacir", "Donald", "Manuel", "Miguel", "Marcus");
+
+echo $funcionarios[0] . "<br>";
+echo $funcionarios[1] . "<br>";
+echo $funcionarios[2] . "<br>";
+echo $funcionarios[3] . "<br>";
+echo $funcionarios[4] . "<br>";
+echo $funcionarios[5] . "<br>";
+echo $funcionarios[6] . "<br>";
+echo $funcionarios[7] . "<br>";
+echo $funcionarios[8] . "<br>";
+echo $funcionarios[9] . "<br>";
+
+
+?>
