fd03b0f2c8a8e46f94b6c0e11c54cdc65aa92a98
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-07 19:50:02 -0200

    Tabela Br

---
+++ revisao/exerciciophp/tabelabrasileiro.php
@@ -0,0 +1,25 @@
+<?php
+
+$times = array("Palmeiras", 
+"Internacinal",
+"Flamengo",
+"São Paulo",
+"Gremio",
+"Atletico-MG",
+"Santos",
+"Cruzeiro",
+"Atletico-PR",
+"Fluminense",
+"Bahia",
+"Corinthians",
+"Vasco da Gama",
+"Botafogo",
+"Ceará SC",
+"Sport Recife",
+"America-MG",
+"Chapecoense",
+"Paraná",);
+
+foreach($times as $time){
+    echo $time."<br>";
+}
