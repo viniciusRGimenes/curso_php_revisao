c19c91bd5810de366c2204b1a080bda354211804
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-07 15:11:37 -0200

    ex5

--- revisao/exerciciophp/exercicio5.php
+++ revisao/exerciciophp/exercicio5.php
@@ -3,8 +3,10 @@
 $tabuada = 1;
 $tabuadaFinal = 10;
 
+$multiplicador = 6;
+
 while($tabuada <= $tabuadaFinal){
-    $resultado = $tabuada  * $tabuadaFinal;
+    $resultado = $tabuada  * $multiplicador;
     echo $tabuada . " X " . $tabuadaFinal . " = ". $resultado . "<br>";
     $tabuada += 1;
 }
