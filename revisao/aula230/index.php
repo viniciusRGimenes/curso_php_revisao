6c4865e7421e191c2ff48bd0205cca3f45ac1423
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-08 19:00:01 -0200

    aula 250

--- revisao/aula230/index.php
+++ revisao/aula230/index.php
@@ -1,6 +1,9 @@
 <form action="processa.php" method="post">
     <input type="text" name="nome" placeholder="Nome"><br><br>
-    <input type="text" name="turma" placeholder="Turma"><br><br>
+    <label>Turma</label><br>
+    <input type="radio" name="turma" value="M">Manhã
+    <input type="radio" name="turma" value="T">Tarde
+    <input type="radio" name="turma" value="N">Noite<br><br>
     <input type="submit" name="enviar dados">
 
 </form>
--- revisao/aula230/processa.php
+++ revisao/aula230/processa.php
@@ -1,6 +1,15 @@
 <?php
-echo "Nome ". $_POST["nome"]. "<br>";
-echo "Turma " . $_POST["turma"];
+echo "Nome completo: ". $_POST["nome"]. "<br>";
+
+if($_POST["turma"]=="M"){
+    echo "Turma Manhã";
+}elseif($_POST["turma"]=="T"){
+    echo "Turma Tarde";
+}elseif($_POST["turma"]=="N"){
+    echo "Turma Noite";
+}else{
+    echo "Não informado";
+}
 
 
 ?>
