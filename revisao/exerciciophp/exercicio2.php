144eb8407424ed3e41411aa6722b1de1594c5706
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-08 17:59:10 -0200

    ex 2

--- revisao/exerciciophp/exercicio2.php
+++ revisao/exerciciophp/exercicio2.php
@@ -1,9 +1,31 @@
 <?php
 
-$numero = 10;
+$numero = 1;
 
-$divisor10 = 10;
-$divisor5 = 5;
-$divisor2 = 2;
+$contador = $numero;
 
-$resto = $numero % 
+$divisao10 = 10;
+$divisao5 = 5;
+$divisao2 = 2;
+
+$resto = $numero % $divisao10;
+
+if($resto == 0 ){
+   
+    echo $numero . " è divisivel por 10". "<br>";
+}
+$resto = $numero % $divisao5;
+if($resto == 0 ){
+   
+    echo $numero . " è divisivel por 5". "<br>";
+}
+$resto = $numero % $divisao2;
+if($resto == 0 ){
+   
+    echo $numero . " è divisivel por 2". "<br>";
+}
+if($resto != 0){
+    echo $numero. " Não é divisivel por nenhum";
+}
+
+?>
