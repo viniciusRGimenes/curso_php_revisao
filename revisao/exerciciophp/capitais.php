80cc23eb85b4a6d2c2bf3eeebff405ab0da8db87
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-08 15:18:03 -0200

    Capitais BR

---
+++ revisao/exerciciophp/capitais.php
@@ -0,0 +1,25 @@
+<?php
+
+$capitais = array("São Paulo",
+"Rio de Janeiro",
+"Belo Horionte",
+"Goiania",
+"Campo Grande",
+"Rio Branco",
+"Boa Vista",
+"Fortaleza",
+"Natal",
+"Salvador",
+);
+
+echo current($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+echo next($capitais). "<br>";
+?>
