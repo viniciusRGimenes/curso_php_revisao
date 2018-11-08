60b622c09e1c27e51d2feb4c025f0547e21e5dfb
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-08 18:37:03 -0200

    aula 230

---
+++ revisao/aula230/index.php
@@ -0,0 +1,6 @@
+<form action="processa.php" method="get">
+    <input type="text" name="nome" placeholder="Nome"><br><br>
+    <input type="text" name="turma" placeholder="Turma"><br><br>
+    <input type="submit" name="enviar dados">
+
+</form>
---
+++ revisao/aula230/processa.php
@@ -0,0 +1,6 @@
+<?php
+echo "Nome ". $_GET["nome"]. "<br>";
+echo "Turma " . $_GET["turma"];
+
+
+?>
