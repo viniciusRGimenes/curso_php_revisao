043d1c41ce761e3ad3c624e51a1bf7d9d71f496a
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-08 16:09:59 -0200

    array associativo

---
+++ revisao/exerciciophp/arrayassociativo.php
@@ -0,0 +1,15 @@
+<?php
+
+$cidade = array(
+    "nome" => "São Paulo",
+    "aniversario" => "25 de janeiro",
+    "fundacao" => "1554 (464 anos)",
+    "gentilico" => "Paulistano",
+    "lema" => "Non dvcor dvco",
+    "prefeito" => "Bruno Covas",
+    "padroeiro" => "São Paulo",
+);
+
+echo $cidade["nome"];
+
+?>
