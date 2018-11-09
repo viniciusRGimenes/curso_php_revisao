3ddebe117825fae9a3a9c7db2a569e5881b9913d
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-09 15:29:47 -0200

    aula 340

--- revisao/aula330/index.php
+++ revisao/aula330/index.php
@@ -1,5 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tarefas</title>
+    <style>
+        table{
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width:100%;
+        }
+        th, td{
+            border:1px solid black;
+            text-align:left;
+            padding:8px;
+        }
+        tr:nth-child(even){
+            background-color:silver;
+        }
 
+    </style>
+</head>
+<body>
+
+<?php
 $servidor = "revisao_db_1";
 $usuario = "root";
 $senha = "phprs";
@@ -15,13 +37,27 @@ $result = $conn->query($sql);
 
 if($result->num_rows > 0 ){
     echo "Total de registros: ". $result->num_rows ."<br>"."<br>";
+    echo "<table>";
+    echo"<tr><th>Id</th><th>Nome</th><th>Status</th></tr>";
 
 
     while($row = $result->fetch_assoc()){
-        echo $row["id"]. "<br>";
-        echo $row["nome"]."<br>";
-        echo $row["status"]."<br>";
+
+        echo "<tr>";
+
+        echo "<td>". $row["id"]. "</td>"    ;
+        echo "<td>". $row["nome"]."</td>";
+        echo "<td>". $row["status"]."</td>";
+
+        echo "</tr>";
     }
+        echo "</table>";
 }else{
     echo "Nehum registro(s) encontrado(s)";
 }
+?>
+
+</body>
+</html>
+
+
