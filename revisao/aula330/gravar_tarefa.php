394bcc818471552f913de65d402dced15c60b035
Vinicius R gimenes <vinicius11_gimenes@hotmail.com>
2018-11-10 21:49:47 -0200

    aula 380

--- revisao/aula330/gravar_tarefa.php
+++ revisao/aula330/gravar_tarefa.php
@@ -1,4 +1,26 @@
 <?php
+$servidor = "revisao_db_1";
+$usuario = "root";
+$senha = "phprs";
+$banco = "curso_php";
 
-print("<pre>");
-var_dump($_POST);
+$nome = $_POST["nome"];
+$detalhes = $_POST["detalhes"];
+$status = $_POST["status"];
+
+$conn = new mysqli($servidor, $usuario, $senha, $banco);
+if($conn->connect_error){
+    die("A conexão falhou". $conn->connect_error);
+}
+
+$sql = "INSERT INTO tarefas (nome, detalhes,status) VALUES ( '$nome', '$detalhes', '$status')";
+
+if($conn->query($sql)===TRUE){
+    echo "Dados Inseridos com sucesso!"."<br>";
+}else{
+    echo "Erro" . $conn_error;
+}
+$conn->close();
+
+?>
+<a href="index.php">Voltar</a>
